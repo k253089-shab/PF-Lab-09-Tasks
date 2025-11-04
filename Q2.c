590cbd4078f69b64be2940ce5b7eea8d35273314
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-11-04 16:38:06 +0500

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void average(int math, int eng, int phy);
+
+int main()
+{
+    int math, eng, phy, avg;
+    printf("Enter marks(0-100 scale):-\n");
+    printf("Mathematics: ");
+    scanf("%d", &math);
+    printf("English: ");
+    scanf("%d", &eng);
+    printf("Physics: ");
+    scanf("%d", &phy);
+    
+    average(math, eng, phy);
+
+    return 0;
+}
+
+void average(int math, int eng, int phy){
+    int avg=(math+eng+phy)/3;
+    printf("\nAverage marks: %d\n", avg);
+    
+    if(avg>=50 && math>=40 && eng>=40 && phy>=40){
+        printf("PASS!\n");
+    } else{
+        printf("FAIL\n");
+        if(math<40)
+        printf("- Marks below 40 in Mathematics\n");
+        if(eng<40)
+        printf("- Marks below 40 in English\n");
+        if(phy<40)
+        printf("- Marks below 40 in Physics\n");
+        if(avg<50)
+        printf("- Average marks below 50\n");
+    }
+}

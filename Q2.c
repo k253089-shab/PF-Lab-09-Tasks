d9e67367b34bd1780eb5753f2e461dc7e7157888
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-11-04 16:40:44 +0500

    Create Q2.c

--- Q2.c
+++ Q2.c
@@ -23,9 +23,9 @@ void average(int math, int eng, int phy){
     printf("\nAverage marks: %d\n", avg);
     
     if(avg>=50 && math>=40 && eng>=40 && phy>=40){
-        printf("PASS!\n");
+        printf("\nPASS!\n");
     } else{
-        printf("FAIL\n");
+        printf("\nFAIL\n");
         if(math<40)
         printf("- Marks below 40 in Mathematics\n");
         if(eng<40)
